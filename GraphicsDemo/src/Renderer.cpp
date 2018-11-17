0e37b49f7cb7eb0fc4b12d9efbdcd76c56c336bf
Hasan <has_b_a@live.co.uk>
2018-11-17 22:58:06 +0000

    Brought in tutorial 13 (cube mapping) content into the scene with FPS counter

--- GraphicsDemo/src/Renderer.cpp
+++ GraphicsDemo/src/Renderer.cpp
@@ -1,60 +1,128 @@
 #include "Renderer.h"
 
-Renderer::Renderer(Window &parent, Timer* timer) : 
-	OGLRenderer(parent), 
-	timer(timer) 
+Renderer::Renderer(Window &parent, Timer* timer) : OGLRenderer(parent), timer(timer) 
 {
-	camera = new Camera();
+	//LOADING
+	reflectShader = new Shader(SHADERDIR"PerPixelVertex.glsl",
+		SHADERDIR"ReflectFragment.glsl");
+	skyboxShader = new Shader(SHADERDIR"SkyboxVertex.glsl",
+		SHADERDIR"SkyboxFragment.glsl");
+	lightShader = new Shader(SHADERDIR"PerPixelVertex.glsl",
+		SHADERDIR"PerPixelFragment.glsl");
+	textTextureShader = new Shader(SHADERDIR"TexturedVertex.glsl", 
+		SHADERDIR"TexturedFragment.glsl");
 
-	currentShader = new Shader(SHADERDIR"TexturedVertex.glsl", SHADERDIR"TexturedFragment.glsl");
-	if (!currentShader->LinkProgram())
-	{
-		//Report Error in Shader
+	if (!reflectShader->LinkProgram() || 
+		!lightShader->LinkProgram()	||	 
+		!skyboxShader->LinkProgram() ||
+		!textTextureShader->LinkProgram())
 		return;
-	}
-
+	
 	basicFont = new Font(SOIL_load_OGL_texture(TEXTUREDIR"tahoma.tga", SOIL_LOAD_AUTO, 
 							SOIL_CREATE_NEW_ID, SOIL_FLAG_COMPRESS_TO_DXT), 16, 16);
-	//TODO: Check font loading 
+	
+	cubeMap = SOIL_load_OGL_cubemap(
+		TEXTUREDIR"rusted_west.jpg", TEXTUREDIR"rusted_east.jpg",
+		TEXTUREDIR"rusted_up.jpg", TEXTUREDIR"rusted_down.jpg",
+		TEXTUREDIR"rusted_south.jpg", TEXTUREDIR"rusted_north.jpg",
+		SOIL_LOAD_RGB, SOIL_CREATE_NEW_ID, 0
+	);
+	
+	heightMap = new HeightMap(TEXTUREDIR"/terrain.raw");
+	heightMap->SetTexture(SOIL_load_OGL_texture(TEXTUREDIR"Barren Reds.JPG",
+		SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS));
+	heightMap->SetBumpMap(SOIL_load_OGL_texture(TEXTUREDIR"Barren RedsDOT3.JPG",
+		SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS));
 
+	quad = Mesh::GenerateQuad();
+	quad->SetTexture(SOIL_load_OGL_texture(TEXTUREDIR"water.TGA",
+		SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS));
+	
+	if (!cubeMap || 
+		!heightMap->GetTexture() ||
+		!heightMap->GetBumpMap() ||
+		!quad->GetTexture())
+		return;
+
+	SetTextureRepeating(quad->GetTexture(), true);
+	SetTextureRepeating(heightMap->GetTexture(), true);
+	SetTextureRepeating(heightMap->GetBumpMap(), true);
+
+	//INITIALISING
+	projMatrix = Matrix4::Perspective(1.0f, 10000.0f, (float)width / (float)height, 45.0f);
+
+	camera = new Camera();
+	camera->SetPosition(Vector3(RAW_WIDTH*HEIGHTMAP_X / 2.0f, 500.0f, RAW_WIDTH*HEIGHTMAP_X));
+
+	light = new Light(Vector3((RAW_WIDTH*HEIGHTMAP_X / 2.0f), 500.0f, (RAW_HEIGHT*HEIGHTMAP_Z / 2.0f)),
+		Vector4(0.9f, 0.9f, 1.0f, 1.0f), (RAW_WIDTH*HEIGHTMAP_X) / 2.0f);
+
+	fpsTextMesh = new TextMesh(fpsText, *basicFont);
+	fpsTextPosition = Vector3(25.0f, 25.0f, 0.0f);
+	
 	init = true;
 	isStatsActive = true;
 
-	fpsTextPosition = Vector3(25.0f, 25.0f, 0.0f);
+	waterRotate = 0.0f;
 
+	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
 }
 
 Renderer::~Renderer(void) 
 {
-	delete camera;
 	delete basicFont;
+	delete fpsTextMesh;
+	delete camera;
+	delete heightMap;
+	delete quad;
+	delete reflectShader;
+	delete skyboxShader;
+	delete lightShader;
+	delete light;
+	delete root;
+
+	currentShader = 0;
 }
 
 void Renderer::UpdateScene(float msec) 
 {
-	camera->UpdateCamera(msec);
-
 	fps = roundf(timer->GetFPS() * 10) / 10;
 	fpsText = "FPS: " + (to_string((short)fps));
+
+	waterRotate += msec / 1000.0f;
+
+	camera->UpdateCamera(msec);
+	viewMatrix = camera->BuildViewMatrix();
+	frameFrustum.FromMatrix(projMatrix * viewMatrix);
+
+	//root->Update(msec);
 }
 
 void Renderer::RenderScene() 
 {
 	timer->Update();
 
+	//BuildNodeLists(root);
+	//SortNodeLists();
+
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glUseProgram(currentShader->GetProgram());
-	glUniform1i(glGetUniformLocation(currentShader->GetProgram(), "diffuseTex"), 0);
-
+	DrawSkybox();
+	DrawHeightMap();
+	
 	if (isStatsActive)
 		DrawStats();
+	
+	DrawWater();
 
-	glUseProgram(0);
+	//DrawNodes();
 
+	//glUseProgram(0);
 	SwapBuffers();
+	//ClearNodeLists();
 }
 
 #pragma region Stats Display
@@ -67,6 +135,11 @@ void Renderer::ToggleStats()
 void Renderer::DrawStats(const float size) 
 {
 	TextMesh* mesh = new TextMesh(fpsText, *basicFont);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+	glDepthMask(GL_FALSE);
+	
+	SetCurrentShader(textTextureShader);
+	glUniform1i(glGetUniformLocation(currentShader->GetProgram(), "diffuseTex"), 0);
 
 	modelMatrix = Matrix4::Translation(Vector3(fpsTextPosition.x, height - fpsTextPosition.y, fpsTextPosition.z)) * Matrix4::Scale(Vector3(size, size, 1));
 	viewMatrix.ToIdentity();
@@ -75,7 +148,147 @@ void Renderer::DrawStats(const float size)
 	UpdateShaderMatrices();
 	mesh->Draw();
 
+	viewMatrix = camera->BuildViewMatrix();
+	projMatrix = Matrix4::Perspective(1.0f, 10000.0f, (float)width / (float)height, 45.0f);
+
+	UpdateShaderMatrices();
+	
+	glUseProgram(0);
+
+	glDepthMask(GL_TRUE);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	delete mesh;
+}
+
+#pragma endregion
+
+#pragma region Scene Node
+
+void Renderer::BuildNodeLists(SceneNode* from)
+{
+	if (frameFrustum.InsideFrustum(*from))
+	{
+		Vector3 dir = from->GetWorldTransform().GetPositionVector() - camera->GetPosition();
+		from->SetCameraDistance(Vector3::Dot(dir, dir));
+
+		nodeList.push_back(from);
+	}
+
+	for (vector<SceneNode*>::const_iterator i = from->GetChildIteratorStart(); i != from->GetChildIteratorEnd(); ++i)
+	{
+		BuildNodeLists((*i));
+	}
+}
+
+void Renderer::SortNodeLists()
+{
+	std::sort(nodeList.begin(), nodeList.end(), SceneNode::CompareByCameraDistance);
+}
+
+void Renderer::DrawNodes()
+{
+	for (vector<SceneNode*>::const_iterator i = nodeList.begin(); i != nodeList.end(); ++i)
+	{
+		DrawNode((*i));
+	}
+}
+
+void Renderer::DrawNode(SceneNode* n)
+{
+	if (n->GetMesh())
+	{
+		glUniformMatrix4fv(glGetUniformLocation(currentShader->GetProgram(), "modelMatrix"),
+			1, false, (float*)&(n->GetWorldTransform()*Matrix4::Scale(n->GetModelScale())));
+
+		glUniform4fv(glGetUniformLocation(currentShader->GetProgram(), "nodeColour"),
+			1, (float*)&n->GetColour());
+
+		glUniform1i(glGetUniformLocation(currentShader->GetProgram(), "useTexture"),
+			(int)n->GetMesh()->GetTexture());
+
+		n->Draw(*this);
+	}
+}
+
+void Renderer::ClearNodeLists()
+{
+	nodeList.clear();
+}
+
+#pragma endregion
+
+#pragma region Scene Contents
+
+void Renderer::DrawSkybox()
+{
+	glDepthMask(GL_FALSE);
+	SetCurrentShader(skyboxShader);
+
+	UpdateShaderMatrices();
+	quad->Draw();
+
+	glUseProgram(0);
+	glDepthMask(GL_TRUE);
+}
+
+void Renderer::DrawHeightMap()
+{
+	SetCurrentShader(lightShader);
+	SetShaderLight(*light);
+
+	glUniform3fv(glGetUniformLocation(currentShader->GetProgram(),
+		"cameraPos"), 1, (float*)&camera->GetPosition());
+
+	glUniform1i(glGetUniformLocation(currentShader->GetProgram(),
+		"diffuseTex"), 0);
+	glUniform1i(glGetUniformLocation(currentShader->GetProgram(),
+		"bumpTex"), 1);
+
+	modelMatrix.ToIdentity();
+	textureMatrix.ToIdentity();
+
+	UpdateShaderMatrices();
+
+	heightMap->Draw();
+
+	glUseProgram(0);
+}
+
+void Renderer::DrawWater()
+{
+	
+
+	SetCurrentShader(reflectShader);
+	SetShaderLight(*light);
+
+	glUniform3fv(glGetUniformLocation(currentShader->GetProgram(),
+		"cameraPos"), 1, (float*)&camera->GetPosition());
+
+	glUniform1i(glGetUniformLocation(currentShader->GetProgram(),
+		"diffuseTex"), 0);
+	glUniform1i(glGetUniformLocation(currentShader->GetProgram(),
+		"cubeTex"), 2);
+
+	glActiveTexture(GL_TEXTURE2);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, cubeMap);
+
+	float heightX = (RAW_WIDTH*HEIGHTMAP_X / 2.0f);
+	float heightY = 256 * HEIGHTMAP_Y / 3.0f;
+	float heightZ = (RAW_HEIGHT*HEIGHTMAP_Z / 2.0f);
+
+	modelMatrix =
+		Matrix4::Translation(Vector3(heightX, heightY, heightZ)) *
+		Matrix4::Scale(Vector3(heightX, 1, heightZ)) *
+		Matrix4::Rotation(90, Vector3(1.0f, 0.0f, 0.0f));
+
+	textureMatrix = Matrix4::Scale(Vector3(10.0f, 10.0f, 10.0f)) *
+		Matrix4::Rotation(waterRotate, Vector3(0.0f, 0.0f, 1.0f));
+
+	UpdateShaderMatrices();
+
+	quad->Draw();
+
+	glUseProgram(0);
 }
 
 #pragma endregion
--- GraphicsDemo/src/Renderer.h
+++ GraphicsDemo/src/Renderer.h
@@ -3,33 +3,63 @@
 #include "Timer.h"
 #include "../../ext/nclgl/OGLRenderer.h"
 #include "../../ext/nclgl/Camera.h"
+#include "../../ext/nclgl/HeightMap.h"
+#include "../../ext/nclgl/SceneNode.h"
+#include "../../ext/nclgl/Frustum.h"
 #include "../../ext/nclgl/TextMesh.h"
+#include <algorithm>
 #include <string>
 #include <math.h>
 
 class Renderer : public OGLRenderer
 {
 public:
-					Renderer(Window &parent, Timer* timer);
-	virtual			~Renderer(void);
+						Renderer(Window &parent, Timer* timer);
+	virtual				~Renderer(void);
 
-	virtual void	UpdateScene(float msec);
-	virtual void	RenderScene();
+	virtual void		UpdateScene(float msec);
+	virtual void		RenderScene();
 
-	void			ToggleStats();
-
-	void			DrawStats(const float size = 16.0f);
+	void				ToggleStats();
+	void				DrawStats(const float size = 16.0f);
 
 protected:
-	Timer*			timer;
-	Camera*			camera;
+	//Rendering/Culling Related
+	Camera*				camera;
+	Frustum				frameFrustum;
 
-	//Stats display variables
-	Font*			basicFont;
-	bool			isStatsActive;
-	float			fps;
-	string			fpsText;
-	Vector3			fpsTextPosition;
+	//Scene Management Related
+	void				BuildNodeLists(SceneNode* from);
+	void				SortNodeLists();
+	void				DrawNodes();
+	void				DrawNode(SceneNode* n);
+	void				ClearNodeLists();
+	
+	SceneNode*			root;
+	vector<SceneNode*>	nodeList;
 
+	//Stats Related
+	Timer*				timer;
+	Font*				basicFont;
+	Shader*				textTextureShader;
+	bool				isStatsActive;
+	TextMesh*			fpsTextMesh;
+	float				fps;
+	string				fpsText;
+	Vector3				fpsTextPosition;
+
+	//Scene Content Related
+	void				DrawHeightMap();
+	void				DrawWater();
+	void				DrawSkybox();
+
+	Shader*				lightShader;
+	Shader*				reflectShader;
+	Shader*				skyboxShader;
+	HeightMap*			heightMap;
+	Mesh*				quad;
+	Light*				light;
+	GLuint				cubeMap;
+	float				waterRotate;
 };
 
